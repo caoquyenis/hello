b0527aed5bddcb8d7a72fc5d8cc52fd1024cafc7
caoquyenis <58635793+caoquyenis@users.noreply.github.com>
2022-05-16 10:15:57 +0700

    master

--- sql_queries.py
+++ sql_queries.py
@@ -8,13 +8,13 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songsplays_table_create = """CREATE TABLE IF NOT EXISTS songsplays (songsplay_id int,start_time float,user_id int,level int,song_id int,artists_id int,session_id int,location varchar,user_agent varchar);"""
+songsplays_table_create = """CREATE TABLE IF NOT EXISTS songsplays (songsplay_id varchar,start_time float,user_id varchar,level varchar,song_id varchar,artists_id varchar,session_id varchar,location varchar,user_agent varchar);"""
 
-users_table_create = """CREATE table if not exists users (user_id int,first_name varchar,last_name varchar,gender varchar,level int);"""
+users_table_create = """CREATE table if not exists users (user_id varchar,first_name varchar,last_name varchar,gender varchar,level varchar);"""
 
-songs_table_create = """CREATE table if not exists songs (songs_id int,title varchar,artists_id int,year int,duration float);"""
+songs_table_create = """CREATE table if not exists songs (song_id varchar,title varchar,artist_id varchar,year int,duration float);"""
 
-artists_table_create = """CREATE table if not exists artists (artists_id int,name varchar,location varchar,latitube float,longitude float);
+artists_table_create = """CREATE table if not exists artists (artist_id varchar,artist_name varchar,artist_location varchar,artist_latitude float,artist_longitude float);
 """
 
 time_table_create = """CREATE table if not exists time (start_time float,hour int,day int,week int,month int,year int,weekday int);"""
@@ -27,13 +27,9 @@ songsplays_table_insert = ("""
 users_table_insert = ("""
 """)
 
-songs_table_insert = (""""INSERT INTO songs (song_id, artist_id, year, duration) \
-                 VALUES (%s, %s, %s, %s)"
-""")
+songs_table_insert = ("""INSERT INTO songs(song_id, artist_id, year, duration) VALUES (%s, %s, %s, %s)""")
 # Select columns for artist ID, name, location, latitude, and longitude
-artists_table_insert = (""""INSERT INTO artists (artist_id, name, location, latitude, longitude) \
-                 VALUES (%s, %s, %s, %s, %s)"
-""")
+artists_table_insert = ("""INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude) VALUES (%s, %s, %s, %s, %s)""")
 
 
 time_table_insert = ("""
